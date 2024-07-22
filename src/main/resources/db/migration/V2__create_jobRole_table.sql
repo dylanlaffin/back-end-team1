40ac8ca0373146b7adcbde89410262d38dd47316
RaggedyRach <rachaelnolan@gmail.com>
2024-07-22 16:30:30 +0100

    fixed Enum structure in create jobRole_table

--- src/main/resources/db/migration/V2__create_jobRole_table.sql
+++ src/main/resources/db/migration/V2__create_jobRole_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE jobRole (
     jobRoleID smallint PRIMARY KEY AUTO_INCREMENT NOT NULL,
     jobRoleName varchar(100) NOT NULL,
-    jobRoleLocation ENUM ("BELFAST", "LONDON", "ç", "BIRMINGHAM", "GDANSK"),
+    jobRoleLocation ENUM ("BELFAST", "LONDON", "TORONTO", "BIRMINGHAM", "GDANSK"),
 	jobRoleCapability ENUM ("HR", "DELIVERY", "MARKETING", "SALES"),
-	jobRoleBand ENUM("BAND1","BAND1","BAND3","BAND4"),
+	jobRoleBand ENUM("BAND1","BAND2","BAND3","BAND4"),
 	jobRoleClosingDate date NOT NULL
 );
 
@@ -12,6 +12,6 @@ INSERT into jobRole
 values
 ("testJobName1","BELFAST","HR","BAND1","2024-07-31"),
 ("testJobName2","BELFAST","DELIVERY","BAND1","2024-07-31"),
-("testJobName3","√","DELIVERY","BAND2","2024-07-31"),
+("testJobName3","TORONTO","DELIVERY","BAND2","2024-07-31"),
 ("testJobName4","BIRMINGHAM","Marketing","BAND1","2024-08-15"),
 ("testJobName5","GDANSK","Sales","BAND3","2024-07-15");
