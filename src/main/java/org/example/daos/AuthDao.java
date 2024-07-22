de6da76f6e26c4bbf2f741bdd32f587040fbf76e
Gerard McLean <gerard.mclean@kainos.com>
2024-07-22 16:14:09 +0100

    Set up AuthDao

--- src/main/java/org/example/daos/AuthDao.java
+++ src/main/java/org/example/daos/AuthDao.java
@@ -1,5 +1,37 @@
 package org.example.daos;
 
-public class AuthDao {
+import org.example.models.LoginRequest;
+import org.example.models.User;
+import org.mindrot.jbcrypt.BCrypt;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AuthDao {
+    public User getUser(final LoginRequest login) throws SQLException {
+        try (Connection conn = DatabaseConnector.getConnection()) {
+            String query = "SELECT Username, Password, RoleID FROM `User` "
+                    + "WHERE Username = ? AND Password = ?";
+
+            PreparedStatement ps = conn.prepareStatement(query);
+
+            ps.setString(1, login.getUsername());
+            String pwHashed = BCrypt.hashpw(login.getPassword(),
+                    BCrypt.gensalt(10));
+            ps.setString(2, pwHashed);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return new User(
+                        rs.getString("Username"),
+                        rs.getString("Password"),
+                        rs.getInt("RoleID")
+                );
+            }
+        }
+        return null;
+    }
 }
