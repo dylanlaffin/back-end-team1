26534deca64a45e6ab470ab5830be612b5f18376
RaggedyRach <rachaelnolan@gmail.com>
2024-07-22 15:33:58 +0100

    added select statement to jobRoleDAO

--- src/main/java/org/example/daos/JobRoleDao.java
+++ src/main/java/org/example/daos/JobRoleDao.java
@@ -1,10 +1,42 @@
 package org.example.daos;
 
+import org.example.models.Band;
+import org.example.models.Capability;
+import org.example.models.JobRole;
+import org.example.models.Location;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JobRoleDao {
 
     /**
      * DAO method to getJobRoles from the database
      *
      */
-    public List<String>
+    public List<JobRole> getAllJobRoles() throws SQLException {
+        List<JobRole> jobRoles = new ArrayList<>();
+
+        try(Connection connection = DatabaseConnector.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    "Select jobRoleID, jobRoleName, jobRoleLocation, jobRoleCapability, jobRoleBand, jobRoleClosingDate from `jobRole`;");
+            while(resultSet.next()) {
+                JobRole jobRole = new JobRole(
+                        resultSet.getInt("jobRoleID"),
+                        resultSet.getString("jobRoleName"),
+                        Location.valueOf(resultSet.getString("jobRoleLocation")),
+                        Capability.valueOf(resultSet.getString("jobRoleCapability")),
+                        Band.valueOf(resultSet.getString("jobRoleBand")),
+                        resultSet.getDate("jobRoleClosingDate"));
+
+                jobRoles.add(jobRole);
+            }
+        }
+        return jobRoles;
+    }
 }
