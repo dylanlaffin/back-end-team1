0c9f3e15081ec2cdb80dfc864b3684373be616a7
Gerard McLean <gerard.mclean@kainos.com>
2024-07-23 20:40:11 +0100

    Migration to create job role table

---
+++ src/main/resources/db/migration/V2__create_job_role_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE jobRole (
+    jobRoleID smallint PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    jobRoleName varchar(100) NOT NULL,
+    jobRoleLocation ENUM ("BELFAST", "LONDON", "TORONTO", "BIRMINGHAM", "GDANSK"),
+	jobRoleCapability ENUM ("HR", "DELIVERY", "MARKETING", "SALES"),
+	jobRoleBand ENUM("BAND1","BAND2","BAND3","BAND4"),
+	jobRoleClosingDate date NOT NULL
+);
+
+INSERT into jobRole
+(jobRoleName, jobRoleLocation, jobRoleCapability, jobRoleBand, jobRoleClosingDate)
+values
+("testJobName1","BELFAST","HR","BAND1","2024-07-31"),
+("testJobName2","BELFAST","DELIVERY","BAND1","2024-07-31"),
+("testJobName3","TORONTO","DELIVERY","BAND2","2024-07-31"),
+("testJobName4","BIRMINGHAM","Marketing","BAND1","2024-08-15"),
+("testJobName5","GDANSK","Sales","BAND3","2024-07-15");
