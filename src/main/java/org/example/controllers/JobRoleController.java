420a374869fd0e4ff1bba62be60cb6fd220bf66d
Gerard McLean <gerard.mclean@kainos.com>
2024-07-26 09:22:46 +0100

    Fixed linting error in UserRole model

--- src/main/java/org/example/controllers/JobRoleController.java
+++ src/main/java/org/example/controllers/JobRoleController.java
@@ -1,15 +1,21 @@
 package org.example.controllers;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.example.exceptions.DatabaseConnectionException;
+import org.example.models.UserRole;
 import org.example.services.JobRoleService;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.SQLException;
+import java.util.List;
 
 @Api("Team1-API")
 @Path("/api/openJobRoles")
@@ -26,6 +32,12 @@ public class JobRoleController {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({UserRole.ADMIN, UserRole.USER})
+    @ApiOperation(
+            value = "Returns all open job roles",
+            authorizations = @Authorization(value = HttpHeaders.AUTHORIZATION),
+            response = List.class
+    )
     public Response getJobRoles() {
         try {
             return Response.ok().entity(
