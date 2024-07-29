34a0379ffcbbf7dcf9524aff886a963c95ca96bc
RaggedyRach <rachaelnolan@gmail.com>
2024-07-29 11:49:07 +0100

    added insert statement to alter jobROle table, removed insert from create jobRole table

--- src/main/resources/db/migration/V7__alter_src_jobRole_table.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE jobRole
-ADD COLUMN jobRoleSpecUrl VARCHAR(2083) AFTER `jobRoleName`,
-ADD COLUMN jobRoleResponsibilities text AFTER `jobRoleSpecUrl`,
-ADD COLUMN jobRoleDescription text AFTER `jobRoleResponsibilities`;
