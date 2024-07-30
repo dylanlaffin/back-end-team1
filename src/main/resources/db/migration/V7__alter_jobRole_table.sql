b367ad360651e9a4ae86d13a74d48e5bf030bda6
RaggedyRach <rachaelnolan@gmail.com>
2024-07-30 09:01:27 +0100

    fixed some spelling errors in test data

--- src/main/resources/db/migration/V7__alter_jobRole_table.sql
+++ src/main/resources/db/migration/V7__alter_jobRole_table.sql
@@ -9,9 +9,9 @@ jobRoleDescription ,jobRoleLocation, capabiltyID, bandID, jobRoleClosingDate, jo
 values
 ("Technical Architect",
 "https://kainos.wd3.myworkdayjobs.com/en-US/Kainos/details/Technical-Architect--Dynamics-365-_JR_14423",
-"test Responsibilites for the technical artchitect role", "test jd for teh arch role", "TORONTO", 4, 2, "2024-08-21", true),
+"test responsibilities for the technical architect role", "test jd for the arch role", "TORONTO", 4, 2, "2024-08-21", true),
 ("Product Manager", "https://kainos.wd3.myworkdayjobs.com/en-US/Kainos/details/Product-Manager_JR_14352",
-"test Responsibiltie for pm", "test description for PM job role","BELFAST", 4, 3, "2024-08-31", true),
+"test responsibilities for pm", "test description for PM job role","BELFAST", 4, 3, "2024-08-31", true),
 ("Sales Development Representative",
 "https://kainos.wd3.myworkdayjobs.com/en-US/Kainos/details/Sales-Development-Representative---Workday_JR_14420",
-"Test responsibilties for sales devel rep", "test description for sales dev rep", "BELFAST", 2, 1, "2024-09-02", true);
+"Test responsibilities for sales devel rep", "test description for sales dev rep", "BELFAST", 2, 1, "2024-09-02", true);
